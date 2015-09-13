ec1aaebcf0b16e94a8b70cb0e4c6da058bfcb59b
Kim Schlesinger <schlesinger.kim@gmail.com>
2015-09-13 10:08:10 -0600

    create and read routes and views working

--- routes/list.js
+++ routes/list.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var db = require('monk')('localhost/list-demo');
+var listCollection = db.get('list');
 
-/* GET home page. */
-router.get('/list', function(req, res, next) {
-  res.render('list/index');
+router.get('/list', function (req, res, next) {
+  listCollection.find({}, function(err, records) {
+    res.render('list/index', {allPosts: records});
+  });
 });
+
+router.get('/list/new', function (req, res, next) {
+  res.render('list/new');
+});
+
+
+router.post('/list', function (req, res, next) {
+  listCollection.insert(
+    {title: req.body.title,
+    author: req.body.author,
+    url: req.body.url,
+    post: req.body.post,
+    comments: []
+  });
+  res.redirect('/list');
+});
+
+
 
 module.exports = router;
